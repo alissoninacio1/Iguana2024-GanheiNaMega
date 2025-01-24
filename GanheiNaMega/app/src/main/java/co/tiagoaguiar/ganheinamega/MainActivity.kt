480ed8c9ae793918a5e94a40bf065ea940fa6012
alissoninacio1 <alissoninacio96@gmail.com>
2025-01-24 15:18:38 -0700

    initial touch event added

--- GanheiNaMega/app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ GanheiNaMega/app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -2,10 +2,31 @@ package co.tiagoaguiar.ganheinamega
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //where you decided what the app will do
         setContentView(R.layout.activity_main)
+
+        //search for the objects and get their reference
+        val editText: EditText  = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        btnGenerate.setOnClickListener {
+            Log.i("test", "button clicked!")
+        }
+
     }
+
 }
+
+
+
